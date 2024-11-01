0357d72c42cceadc6f613c9dadff55ccdd95d044
Milly Ndeto <milkandeto08@gmail.com>
2024-11-01 12:37:44 +0300

    Intro to Python Assignment

---
+++ PYTHON CLASSES/index.py
@@ -0,0 +1,12 @@
+number = 2
+name = "My name is Milka Mutiso"
+gender = "female"
+print(number, name, gender)
+languages = ["web dev", "JS", "Nodejs", "CSS", 5, True]
+print(languages)
+#print from right
+print(languages[2])
+#count from left
+print(languages[-3])
+
+
---
+++ PYTHON CLASSES/user_input.py
@@ -0,0 +1,13 @@
+# asking for inputs
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where do you live? ")
+
+#user input
+name = "Alice"
+age = "28"
+location = "Nairobi"
+
+# displaying the personalized message
+print(f"Hello, my name is {name}, I am {age} years old and I live in {location}.")
+
